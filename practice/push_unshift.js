0d3b26056d37ab1d82bdb797e09b35b6ea8f99ca
Gautam99-coder <gautamtharu1000@gmail.com>
2025-05-07 09:28:46 +0530

    Create push_unshift.js

---
+++ practice/push_unshift.js
@@ -0,0 +1,5 @@
+//adding Elements with push() and unshift()
+const numbers =[1,2,3,4,5];
+numbers.push(4);//add to end
+numbers.unshift(0); //add to start
+console.log(numbers);// Output: [0,1,2,3,4,5,4]
