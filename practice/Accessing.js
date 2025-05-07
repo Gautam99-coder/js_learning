cb46f8610f244f47f50b08f5d8e637817fc5e103
Gautam99-coder <gautamtharu1000@gmail.com>
2025-05-07 09:25:42 +0530

    Create Accessing.js

---
+++ practice/Accessing.js
@@ -0,0 +1,4 @@
+const fruits = ["apple","banana","orange","mango","charry","Ram-phal"];
+console.log(fruits[0]); // Outputs: apple
+fruits[1] = "grape"; // Modify index 1
+console.log(fruits); // Outputs: ["apple", "grape", "orange","mango","charry","Ram-phal"]
